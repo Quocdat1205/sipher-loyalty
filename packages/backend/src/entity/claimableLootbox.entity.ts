6164f55d66efd99d70f64d30ee828a3566df2d21
vulh1209 <vu.le@sipher.xyz>
2022-03-21 16:39:32 +0700

    chore: add type api claimableLootbox

--- packages/backend/src/entity/claimableLootbox.entity.ts
+++ packages/backend/src/entity/claimableLootbox.entity.ts
@@ -5,25 +5,32 @@ import {
   Entity,
   PrimaryGeneratedColumn,
 } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class ClaimableLootbox {
+  @ApiProperty({ type: Number })
   @PrimaryGeneratedColumn("increment")
   id?: number;
 
+  @ApiProperty({ type: String })
   @Column({ nullable: false })
   publicAddress: string;
 
+  @ApiProperty({ type: Number })
   @Column({ default: 1 })
   quantity: number;
 
+  @ApiProperty({ type: Number })
   @Column({ nullable: false })
   tokenId: number;
 
+  @ApiProperty()
   @Column({ type: "timestamp", nullable: true })
   @Transform(({ value }) => new Date(value))
   expiredDate: Date;
 
+  @ApiProperty()
   @CreateDateColumn({ default: new Date() })
   createdAt?: Date;
 }
