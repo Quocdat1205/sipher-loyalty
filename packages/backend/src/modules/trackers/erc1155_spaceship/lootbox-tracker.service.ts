797fd1b7077328735851c87e532ad566709cf9d6
Hoàng Vũ <vu.le@sipher.xyz>
2022-03-24 19:09:05 +0700

    fix(tracker-lootbox): add tracker canceled

--- packages/backend/src/modules/trackers/erc1155_spaceship/lootbox-tracker.service.ts
+++ packages/backend/src/modules/trackers/erc1155_spaceship/lootbox-tracker.service.ts
@@ -70,7 +70,7 @@ export class LootboxTrackerService {
 
   @Interval("tracking lootbox canceled", 15000)
   async TrackingCanceledInterval() {
-    this.fromBlockBurned = await this.trackingCancel(this.fromBlockCanceled);
+    this.fromBlockCanceled = await this.trackingCancel(this.fromBlockCanceled);
     const trackedBlock = await this.trackedBlockRepo.findOne({
       where: { type: "cancel" },
     });
@@ -335,7 +335,7 @@ export class LootboxTrackerService {
       return _fromBlock; // retry
     }
     LoggerService.log(
-      `Start tracking burned from ${_fromBlock} to ${_currentBlock}`
+      `Start tracking canceled from ${_fromBlock} to ${_currentBlock}`
     );
     const filter = this.contract.filters.CancelOrder();
     const pastEvents = await this.contract
@@ -355,6 +355,7 @@ export class LootboxTrackerService {
       );
     });
     await Promise.all(promises);
+    LoggerService.log(`End tracking minted at ${_currentBlock + 1}`);
     return _currentBlock + 1;
   };
 }
