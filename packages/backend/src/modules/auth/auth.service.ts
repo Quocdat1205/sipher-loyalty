94642199f5f01aaad09bf41406895cd9fe222fcd
Kim Son <sonngo81.99@gmail.com>
2022-03-25 14:42:37 +0700

    feat(auth): return 400 for bad public address

--- packages/backend/src/modules/auth/auth.service.ts
+++ packages/backend/src/modules/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { toChecksumAddress } from "ethereumjs-util";
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 
 import { UserData } from "./auth.types";
+import { isEthereumAddress } from "class-validator";
 
 @Injectable()
 export class AuthService {
   verifyAddress = async (publicAddress: string, userData: UserData) => {
+    if (!isEthereumAddress(publicAddress)) {
+      throw new HttpException("Invalid public address", HttpStatus.BAD_REQUEST);
+    }
     if (
       userData.publicAddress.findIndex(
         (wAddress) => wAddress === toChecksumAddress(publicAddress)
