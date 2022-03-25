5db4edceae2e20015d8609ec154009c83eaaccb3
Hoàng Vũ <vu.le@sipher.xyz>
2022-03-25 11:03:35 +0700

    fix: add cors https://api-loyalty.sipher.gg/

--- packages/backend/src/setting/cors.ts
+++ packages/backend/src/setting/cors.ts
@@ -1,7 +1,11 @@
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 const cors: CorsOptions = {
-  origin: ["http://localhost:3001", "http://localhost:3000"],
+  origin: [
+    "http://localhost:3001",
+    "http://localhost:3000",
+    "https://api-loyalty.sipher.gg",
+  ],
   credentials: true,
 };
 
