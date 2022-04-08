47870c266588fad2b1ac8250e2587c1fdfed385a
Kim Son <sonngo81.99@gmail.com>
2022-04-08 11:40:43 +0700

    feat(health): add health check module

---
+++ packages/backend/src/modules/health/health.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import {
+  HealthCheckService,
+  HttpHealthIndicator,
+  HealthCheck,
+  TypeOrmHealthIndicator,
+} from "@nestjs/terminus";
+import constant from "@setting/constant";
+
+@ApiTags("health")
+@Controller("health")
+class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator
+  ) {}
+
+  @Get("server")
+  serverCheck() {
+    return "ok";
+  }
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () =>
+        this.http.pingCheck(
+          "server",
+          constant.isProduction
+            ? "https://api-loyalty.sipher.gg/api/sipher/loyalty/health/server"
+            : `http://localhost:${constant.PORT}/api/sipher/loyalty/health/server`
+        ),
+      () => this.db.pingCheck("database"),
+    ]);
+  }
+}
+
+export default HealthController;
